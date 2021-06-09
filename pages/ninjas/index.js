605185508889c3a2230330c77271599e54b80675
leo <leo.chen@cloud-interactive.com>
2021-06-09 18:06:12 +0800

    Add get static props

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,30 @@
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+  const ninjas = await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      ninjas,
+    },
+  };
+};
+
+const Ninjas = (props) => {
+  const { ninjas } = props;
+
   return (
     <div>
-      <h1>Ninjas Listing</h1>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
