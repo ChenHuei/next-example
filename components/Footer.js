dbbf44daea1fa2586012f9525a182bc0f2e282c5
leo <leo.chen@cloud-interactive.com>
2021-06-09 17:23:12 +0800

    Add pages

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer>Copyright 2021 Huei</footer>;
+};
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+// components
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className="content">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
